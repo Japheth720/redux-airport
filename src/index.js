1bf97cd44a166084e11070e53e74e9aa923e8ce3
Japheth Yang <japheth720@gmail.com>
2023-05-02 15:10:15 -0500

    first steps done

--- src/index.js
+++ src/index.js
@@ -4,18 +4,36 @@ import App from './components/App/App';
 
 /** TODO: import REDUX **/
 
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
 /** TODO: Add REDUCERS */
+
+const planeCount = (state=[], action) => {
+
+
+}
 
 
 /** TODO: Create store */
 
-
+const planeStore = createStore(
+    combineReducers({
+      planeCount,
+      
+    }),
+    applyMiddleware(
+      logger
+    )
+  );
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={planeStore}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
